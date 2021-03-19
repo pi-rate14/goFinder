b126f6df48985028b7df22e51f4be69712daf9b2
ripunjaynarula <ripunjay.narula2019@vitstudent.ac.in>
2021-03-20 00:30:18 +0530

    navbar

---
+++ Frontend/gofinder/src/Navbar.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button, Navbar, Nav, NavItem, NavDropdown, MenuItem } from 'react-bootstrap';
+const NavBar = () => {
+
+ 
+
+
+  return(
+    <>
+    <div className="navbar">
+  <Navbar bg="light" variant="light">
+    <Navbar.Brand href="#home">goFinder</Navbar.Brand>
+    
+  </Navbar>
+  </div>
+        </>
+    )
+}
+
+export default NavBar;
